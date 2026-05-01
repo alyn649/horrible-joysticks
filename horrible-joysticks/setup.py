c9dd1255c639b11bedc3bf7fb2d413896613a557
Angus Lynch <alyn649@aucklanduni.ac.nz>
2026-05-01 12:25:18 +1200

    Structure for python code

--- horrible-joysticks/setup.py
+++ horrible-joysticks/setup.py
@@ -1,6 +1,13 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'horrible-joysticks'
+
+config_files = [p for p in glob(os.path.join("config", "**", "*"), recursive=True)
+                if os.path.isfile(p)]
+launch_files = [p for p in glob(os.path.join("launch", "**", "*"), recursive=True)
+                if os.path.isfile(p)]
 
 setup(
     name=package_name,
@@ -10,6 +17,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), config_files),
+        (os.path.join('share', package_name, 'launch'), launch_files),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
